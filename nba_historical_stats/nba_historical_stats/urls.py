7de15016ce5d84f132955863e468493522907fc6
gruvis41 <dougheinan@gmail.com>
2023-04-18 17:54:25 -0600

    Updated to be template-able.

--- nba_historical_stats/nba_historical_stats/urls.py
+++ nba_historical_stats/nba_historical_stats/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stat-page', include('stat_page.urls'))
+    path('stat_page/', include('stat_page.urls'))
 ]
--- nba_historical_stats/stat_page/views.py
+++ nba_historical_stats/stat_page/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+players = [
+    {
+        'f_name': 'Michael',
+        'l_name': 'Jordan',
+        'position': 'SG/SF',
+        'career_pts': 32292
+    },
+    {
+        'f_name': 'Lebron',
+        'l_name': 'James',
+        'position': 'SF/PF',
+        'career_pts': 38652
+    }
+]
+
 
 def home(request):
-    return render(request, 'stat_page/stat_page.html')
+    context = {
+        'players': players
+    }
+    return render(request, 'stat_page/stat_page.html', context)
 
 
 def about(request):
