96b4794eb47b9e335ce4f79d7a82367e8facd58d
gruvis41 <dougheinan@gmail.com>
2023-04-29 19:00:50 -0600

    Begun...

--- nba_historical_stats/stat_page/views.py
+++ nba_historical_stats/stat_page/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from nba_api.stats.static import players, teams
+import random
 
-players = [
-    {
-        'f_name': 'Michael',
-        'l_name': 'Jordan',
-        'position': 'SG/SF',
-        'career_pts': 32292
-    },
-    {
-        'f_name': 'Lebron',
-        'l_name': 'James',
-        'position': 'SF/PF',
-        'career_pts': 38652
-    }
-]
+players = players.get_players()
 
 
 def home(request):
     context = {
-        'players': players
+        'players': players,
+        'first_players': players[0:10],
+        'active_players': [player for player in players if player['is_active']],
+        'rando': rando_player(players)
     }
     return render(request, 'stat_page/stat_page.html', context)
 
 
 def about(request):
     return render(request, 'stat_page/about.html', {"title": "About"})
+
+
+def rando_player(player_list):
+    print(len(player_list))
+    total_players = len
+    return "Steven Adams"
+
