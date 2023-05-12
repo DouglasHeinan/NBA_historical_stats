2e4e7c4ab971f7847b1e357fec52f096d51adff9
gruvis41 <dougheinan@gmail.com>
2023-05-12 15:46:38 -0600

    database creates and updatse itself on app start

--- nba_historical_stats/stat_page/static/stat_page/app.js
+++ nba_historical_stats/stat_page/static/stat_page/app.js
@@ -1,4 +1,8 @@
+const randomPlayerBtn = document.querySelector("#randomPlayerReveal");
+randomPlayerBtn.addEventListener("click", revealRandomPlayer)
+
 function revealRandomPlayer() {
     toShow = document.getElementById('randomPlayer');
     toShow.classList.remove('hidden');
 }
+
--- nba_historical_stats/stat_page/views.py
+++ nba_historical_stats/stat_page/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django_pandas.managers import DataFrameManager
 from .models import AllPlayers, AllTeams
+from nba_api.stats.static import players
 import random
 
 
 def home(request):
-    players = AllPlayers.objects.all()
-    print(f'****************************{players}')
-    update_db(players)
+    all_players = AllPlayers.objects.all()
+    if not all_players:
+        createDatabase()
+    update_db()
     context = {
-        'players': players
+        'players': all_players
     }
     return render(request, 'stat_page/stat_page.html', context)
 
@@ -19,7 +21,12 @@ def about(request):
     return render(request, 'stat_page/about.html', {"title": "About"})
 
 
-def rando_player(player_list):
+def rando_player():
+    rand_int = random.randint[0, 4814]
+    rand_player = AllPlayer.objects.get(player_id=rand_int)
+    return rand_player
+
+
     total_players = len(player_list)
     player_index = random.randrange(total_players)
     return player_list[player_index]
@@ -29,14 +36,29 @@ def determine_bb_ref_link(player):
     return f"{player['last_name'][0].lower()}/{(player['last_name'][0:5]).lower()}{player['first_name'][0:2].lower()}01.html"
 
 
-def update_db(players):
-    for player in players:
-        if not AllPlayers.objects.filter(player_id=player.id).first():
+def createDatabase():
+    all_players = players.get_players()
+    for player in all_players:
+        new_player = AllPlayers(
+            player_id=player["id"],
+            first_name=player["first_name"],
+            last_name=player["last_name"],
+            is_active=player["is_active"],
+            bb_ref_link=f"https://www.basketball-reference.com/players/{player['last_name'][0].lower()}/{(player['last_name'][0:5]).lower()}{player['first_name'][0:2].lower()}01.html"
+        )
+        new_player.save()
+
+
+
+def update_db():
+    all_players = players.get_players()
+    for player in all_players:
+        if not AllPlayers.objects.filter(player_id=player["id"]).exists():
             new_player = AllPlayers(
-                player_id=player.id,
-                first_name=player.first_name,
-                last_name=player.last_name,
-                is_active=player.is_active,
+                player_id=player["id"],
+                first_name=player["first_name"],
+                last_name=player["last_name"],
+                is_active=player["is_active"],
                 bb_ref_link=f"https://www.basketball-reference.com/players/{player['last_name'][0].lower()}/{(player['last_name'][0:5]).lower()}{player['first_name'][0:2].lower()}01.html"
             )
             new_player.save()
