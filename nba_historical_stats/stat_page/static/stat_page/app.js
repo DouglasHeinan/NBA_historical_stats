baa59f7c68f1dd238bcb8b8e4c6ae12a07b39566
gruvis41 <dougheinan@gmail.com>
2023-05-13 17:38:10 -0600

    improving randPlayer function...

--- nba_historical_stats/stat_page/static/stat_page/app.js
+++ nba_historical_stats/stat_page/static/stat_page/app.js
@@ -4,11 +4,9 @@ const randomPlayerBtn = document.querySelector("#randomPlayerReveal");
 randomPlayerBtn.addEventListener("click", revealRandomPlayer)
 
 function revealRandomPlayer() {
-    randIndex = Math.floor(Math.random() * 4000) + 1;
-    randPlayer = allPlayers[randIndex];
-    console.log(randPlayer.first_name)
-    return randPlayer
+    toShow = document.getElementById('randomPlayer');
+    toShow.classList.remove('hidden');
 }
 
-//    toShow = document.getElementById('randomPlayer');
-//    toShow.classList.remove('hidden');
+
+
--- nba_historical_stats/stat_page/views.py
+++ nba_historical_stats/stat_page/views.py
@@ -22,14 +22,19 @@ def about(request):
 
 
 def rando_player():
-    rand_int = random.randint[0, 4814]
-    rand_player = AllPlayer.objects.get(player_id=rand_int)
-    return rand_player
+    # // randIndex = Math.floor(Math.random() * 4000) + 1;
+    # // randPlayer = allPlayers[randIndex];
+    # // console.log(randPlayer.first_name)
+    # // return randPlayer
+
+    # rand_int = random.randint[0, 4814]
+    # rand_player = AllPlayer.objects.get(player_id=rand_int)
+    # return rand_player
 
 
-    total_players = len(player_list)
-    player_index = random.randrange(total_players)
-    return player_list[player_index]
+    # total_players = len(player_list)
+    # player_index = random.randrange(total_players)
+    # return player_list[player_index]
 
 
 def determine_bb_ref_link(player):
