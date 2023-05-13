16e8ca97f325d689cc3da00952b7adab62f04908
gruvis41 <dougheinan@gmail.com>
2023-05-13 17:30:25 -0600

    Updated randplayer btn funciton.

--- nba_historical_stats/stat_page/static/stat_page/app.js
+++ nba_historical_stats/stat_page/static/stat_page/app.js
@@ -1,10 +1,13 @@
+{% allPlayers = players %}
+
 const randomPlayerBtn = document.querySelector("#randomPlayerReveal");
 randomPlayerBtn.addEventListener("click", revealRandomPlayer)
 
 function revealRandomPlayer() {
-    document.querySelector("")
+    randIndex = Math.floor(Math.random() * 4000) + 1;
+    randPlayer = allPlayers[randIndex];
+    return randPlayer
+}
 
 //    toShow = document.getElementById('randomPlayer');
 //    toShow.classList.remove('hidden');
-}
-
