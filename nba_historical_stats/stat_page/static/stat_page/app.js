fc98a613921096f201f4480be5636b5c9c100a99
gruvis41 <dougheinan@gmail.com>
2023-05-13 17:25:19 -0600

    Updated javascript code

--- nba_historical_stats/stat_page/static/stat_page/app.js
+++ nba_historical_stats/stat_page/static/stat_page/app.js
@@ -2,7 +2,9 @@ const randomPlayerBtn = document.querySelector("#randomPlayerReveal");
 randomPlayerBtn.addEventListener("click", revealRandomPlayer)
 
 function revealRandomPlayer() {
-    toShow = document.getElementById('randomPlayer');
-    toShow.classList.remove('hidden');
+    document.querySelector("")
+
+//    toShow = document.getElementById('randomPlayer');
+//    toShow.classList.remove('hidden');
 }
 
